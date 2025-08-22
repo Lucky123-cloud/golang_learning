c937fc20e76b5eb1099e59e9e168036fc2c4862b
Lucky-cloud 123 <luckybaraka21@gmail.com>
2025-08-22 19:54:18 +0300

    added the go things

--- golang_w3s/05_go_output_functions/05_go_printf_function.go
+++ golang_w3s/05_go_output_functions/05_go_printf_function.go
@@ -2,6 +2,12 @@ package main
 
 // The Printf() function first formats its argument based on the given formatting verb and then prints them.
 
+// Verb	Description
+// %v	Prints the value in the default format
+// %#v	Prints the value in Go-syntax format
+// %T	Prints the type of the value
+// %%	Prints the % sign
+
 import (
 	"fmt"
 )
@@ -11,7 +17,17 @@ func main() {
 	// %T is used to print the type of the arguments
 	var i string = "Hello"
 	var j int = 15
+	var k = 15.5
+	var text = "Hello World!"
 
 	fmt.Printf("i has value: %v and type: %T\n", i, i)
 	fmt.Printf("j has value: %v and type: %T\n", j, j)
+	fmt.Printf("%v\n", k)
+	fmt.Printf("%#v\n", k)
+	fmt.Printf("%v%%\n", k)
+	fmt.Printf("%T\n", k)
+
+	fmt.Printf("%v\n", text)
+	fmt.Printf("%#v\n", text)
+	fmt.Printf("%T\n", text)
 }
