a96a5e1c40fbe1835a0d4ffefdf18f0e6570f73c
Lucky-cloud 123 <luckybaraka21@gmail.com>
2025-08-22 22:33:59 +0300

    added go data types

---
+++ golang_w3s/06_Data_Types/06_data_types.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name string = "Lucky"
+	var age int = 21
+	var weight float32 = 34.5
+	var isAdult bool = true
+
+	fmt.Println("String: ", name)
+	fmt.Println("Interger: ", age)
+	fmt.Println("Float: ", weight)
+	fmt.Println("Boolean: ", isAdult)
+}
