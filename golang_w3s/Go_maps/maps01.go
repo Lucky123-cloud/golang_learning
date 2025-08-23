2cffd8ec3c3cb7489fc7ebaaf3a5ec590d5aef5c
Lucky-cloud 123 <luckybaraka21@gmail.com>
2025-08-23 19:37:58 +0300

    added the maps

---
+++ golang_w3s/Go_maps/maps01.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = map[string]string{"brand": "Ford", "model": "Mustang", "year": "1964"}
+	b := map[string]int{"Oslo": 1, "Bergen": 2, "Trondheim": 3, "Stavanger": 4}
+
+	fmt.Printf("a: %v\n", a)
+	fmt.Printf("b: %v\n", b)
+
+	//will continue later on
+}
