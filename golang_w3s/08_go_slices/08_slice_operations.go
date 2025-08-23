49a6f0b02202f132bf98bca34a7a0ad3a70fa3c2
Lucky-cloud 123 <luckybaraka21@gmail.com>
2025-08-23 10:32:13 +0300

    added the copy function to manage memory

---
+++ golang_w3s/08_go_slices/08_slice_operations.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//acccessing slice elements
+	prices := []int{10, 20, 30}
+
+	fmt.Println(prices[0])
+	fmt.Println(prices[2])
+
+	//changeing slice elements
+	prices[2] = 50
+	fmt.Println(prices[0])
+	fmt.Println(prices[2])
+
+	//append elements to a slice using the built-in append function
+	myslice1 := []int{1, 2, 3, 4, 5, 6}
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	myslice1 = append(myslice1, 20, 21)
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	//append one slice to another slice
+	myslice2 := []int{7, 8, 9}
+	myslice3 := []int{10, 11, 12}
+	myslice2 = append(myslice2, myslice3...)
+	fmt.Printf("myslice2 = %v\n", myslice2)
+	fmt.Printf("length = %d\n", len(myslice2))
+	fmt.Printf("capacity = %d\n", cap(myslice2))
+
+	//change the length of a slice
+	arr1 := [6]int{9, 10, 11, 12, 13, 14} //an array
+	myslice4 := arr1[1:5]                 // slice array
+	fmt.Printf("myslice4 = %v\n", myslice4)
+	fmt.Printf("length = %d\n", len(myslice4))
+	fmt.Printf("capacity = %d\n", cap(myslice4))
+
+	myslice4 = arr1[1:3] //change length by re-slicing the array
+	fmt.Printf("myslice4 = %v\n", myslice4)
+	fmt.Printf("length = %d\n", len(myslice4))
+	fmt.Printf("capacity = %d\n", cap(myslice4))
+
+	myslice4 = append(myslice4, 20, 21, 22, 23) //change length by appending elements
+	fmt.Printf("myslice4 = %v\n", myslice4)
+	fmt.Printf("length = %d\n", len(myslice4))
+	fmt.Printf("capacity = %d\n", cap(myslice4))
+
+}
