dbe9d365986dd2d42d0fb72643f926bc1fcfc49e
Lucky Baraka <luckybaraka21@gmail.com>
2025-11-19 15:10:42 +0300

    added the git path

---
+++ golang_by_example/variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "Lucky Blessed"
+	fmt.Println(a)
+
+	var b string
+	fmt.Println(b)
+
+	var c int = 7
+	fmt.Println(c)
+
+	f := "Hello World from Golang"
+	fmt.Println(f)
+}
