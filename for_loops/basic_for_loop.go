6bd4e1751413c8478938c68b6895b231cc3551c1
Lucky Baraka <luckybaraka21@gmail.com>
2025-12-10 18:58:15 +0300

    add: added a basic for loop in go

---
+++ for_loops/basic_for_loop.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main() {
+	//for loops in golang acts like the one in c or java
+	//we have a loop initialzer(i := 0), condition(i < 5) post(i++)
+	for i := 0; i < 5; i++ {
+		fmt.Println("Lucky is the best software engineer in the World")
+	}
+}
