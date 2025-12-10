2b4fb6ae3f898b848243561db518e0345e3b7d46
Lucky Baraka <luckybaraka21@gmail.com>
2025-12-10 19:01:46 +0300

    add: added for loop like a while loop

---
+++ for_loops/for_loop_like_while_loop.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//we can also write  a for loop in golang like a while loop
+	i := 0
+	for i < 5 {
+		fmt.Println("Hello World, Lucky is the best software engineer")
+		i++
+	}
+}
+
