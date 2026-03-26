f002b2709c12041ea548be26ee5157a6235d665b
Lucky Baraka <lucky.baraka@savannahinformatics.com>
2026-03-26 09:12:40 +0300

    delete: remove the delete things

---
+++ golang_mastery/dupl.go
@@ -0,0 +1,23 @@
+//here the dup func tries to return the number of inputs that appear more than once
+// in the stdin.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
